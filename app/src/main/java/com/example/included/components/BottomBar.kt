c743df283c4b168f821856e860d5407404081af5
Enzo Pelegrini <113308665+epelegrini@users.noreply.github.com>
2025-08-17 12:56:44 -0300

    Update BottomBar.kt

--- app/src/main/java/com/example/included/components/BottomBar.kt
+++ app/src/main/java/com/example/included/components/BottomBar.kt
@@ -2,55 +2,27 @@ package com.example.included.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.vector.ImageVector
 
-sealed class BottomBarScreen(
-    val route: String,
-    val title: String,
-    //val icon: ImageVector
-) {
-    object Home : BottomBarScreen(
-        route = "home",
-        title = "Home",
-        //icon = Icons.Default.Home
-    )
-
-    object Search : BottomBarScreen(
-        route = "search",
-        title = "Buscar",
-        //icon = Icons.Default.Search
-    )
-
-    object Profile : BottomBarScreen(
-        route = "profile",
-        title = "Perfil",
-        //icon = Icons.Default.Person
-    )
+sealed class BottomBarScreen(val route: String, val title: String, val icon: ImageVector) {
+    object Home : BottomBarScreen("home", "Home", Icons.Default.Home)
+    object Search : BottomBarScreen("search", "Buscar", Icons.Default.Search)
+    object Profile : BottomBarScreen("profile", "Perfil", Icons.Default.Person)
+    object Notifications : BottomBarScreen("notifications", "Notificações", Icons.Default.Notifications)
 }
 
 @Composable
-fun BottomBar(
-    currentRoute: String?,
-    onNavigate: (String) -> Unit
-) {
+fun BottomBar(currentRoute: String?, onNavigate: (String) -> Unit, notificationCount: Int) {
     NavigationBar {
-        listOf(
-            BottomBarScreen.Home,
-            BottomBarScreen.Search,
-            BottomBarScreen.Profile
-        ).forEach { screen ->
+        val screens = listOf(BottomBarScreen.Home, BottomBarScreen.Search, BottomBarScreen.Notifications, BottomBarScreen.Profile)
+        screens.forEach { screen ->
             NavigationBarItem(
-                icon = {
-                    //Icon(
-                        //imageVector = screen.icon,
-                        //contentDescription = screen.title
-                  //  )
-                },
+                icon = { Icon(screen.icon, contentDescription = screen.title) },
                 label = { Text(screen.title) },
                 selected = currentRoute == screen.route,
                 onClick = { onNavigate(screen.route) }
